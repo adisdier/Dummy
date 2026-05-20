3b287e2dab8572d1bc4813264a97cc025ab20712
Alexandre Disdier <alexandre.disdier@estia.fr>
2026-05-20 14:39:41 +0200

    Ai ajouté la fonction ajouter()

--- main.c
+++ main.c
@@ -1,8 +1,12 @@
 #include "stdio.h"
 #include "stdlib.h"
 
+int ajoute(int a, int b) {
+	return a + b;
+}
+
 int main () {
-	printf("Hello Estia!\n");
+	printf("La somme de 2 et 4 est %d\n", ajoute(2, 4));
 
 	return 0;
 }
